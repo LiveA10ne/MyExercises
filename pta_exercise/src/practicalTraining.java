1e1156e997109e2d3719f046635f49926909f9c2
LiveA10ne <sxs991124@gmail.com>
2021-03-15 10:05:35 +0800

    first commit

---
+++ pta_exercise/src/practicalTraining.java
@@ -0,0 +1,13 @@
+/**
+ * @author Somnambulist
+ * @date 2021/3/15 9:12
+ */
+public class practicalTraining {
+    private static void solution(){
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
